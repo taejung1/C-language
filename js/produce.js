7bc6c2206618663641d5e41dde9ffefce6af75e0
taejung1 <timkim123@naver.com>
2022-11-12 23:26:56 +0900

    commit message

---
+++ js/produce.js
@@ -0,0 +1,15 @@
+const input = require('readline-sync').question;
+const fs = require('fs');
+
+const count = input("Please enter the problem number! : ")
+
+const strig = fs.readFileSync('../template.c').toString()
+
+fs.writeFile(`../${count}` , strig ,function(err){
+    if (err === null) {
+        console.log('성공적으로 파일을 생성하였습니다.');
+    } else {
+        console.log('파일 생성을 실패했습니다.');
+    }
+});
+
---
+++ start.bat
@@ -0,0 +1,2 @@
+cd js
+node produce.js
