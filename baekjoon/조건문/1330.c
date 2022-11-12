d72ef71ba54fa5b70586a4c9346d5d09dac54645
김태정 <85492054+taejung1@users.noreply.github.com>
2022-11-12 21:12:24 +0900

    Create 1330.c

---
+++ baekjoon/조건문/1330.c
@@ -0,0 +1,18 @@
+
+#include <stdio.h>
+
+int main()
+{
+    int a, b;
+    
+    scanf("%d %d", &a, &b);
+
+    if (a > b) {
+        printf(">");
+    } else if (a == b) {
+         printf("==");
+    } 
+    else {
+        printf("<");
+    }
+}
