4ed52593390ac5dee3e8dab1ba9d070c01856c68
taejung1 <timkim123@naver.com>
2022-11-12 14:11:54 +0900

    4

---
+++ baekjoon/3003.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+	int n1, n2, n3, n4, n5, n6;
+	int m1, m2, m3, m4, m5, m6;
+	scanf("%d %d %d %d %d %d", &n1, &n2, &n3, &n4, &n5, &n6);
+
+    if(!n1 == 1) {
+		if(n1 > 1) {
+			m1 = n1 - (n1 -1);
+		} else {
+			m1 = n1 - ;
+		}
+	}
+
+	printf("%d" , m1);
+
+	return 0;
+}
+
+//보류
