5315a81e654d645ba7483af0e59823d54837657d
taejung1 <timkim123@naver.com>
2022-11-16 22:16:26 +0900

    update

--- js/produce.js
+++ js/produce.js
@@ -5,8 +5,8 @@ const count = input("Please enter the problem number! : ")
 
 const strig = fs.readFileSync('../template.c').toString()
 
-fs.writeFile(`../${count}.c` , strig ,function(err){
-    if (err === null) {
+fs.writeFile(`../${count}.c` , strig ,function(error){
+    if (error === null) {
         console.log('성공적으로 파일을 생성하였습니다.');
     } else {
         console.log('파일 생성을 실패했습니다.');
