257d6c98622a01ae685f5c3254f143c26b601ec9
taejung1 <timkim123@naver.com>
2022-11-16 22:48:32 +0900

    update

--- baekjoon/함수/4673.c
+++ baekjoon/함수/4673.c
@@ -1,8 +1,28 @@
 #include <stdio.h>
 
+int d(int n)
+{
+    int result = n;
+    while (n > 0)
+    {
+        result += n % 10;
+        n = n / 10;
+    }
+    return result;
+}
 
 int main()
 {
-    // 보류
-    return 0;
+    int list[10001] = {0}, result = 0;
+    for (int i = 0; i < 10001; i++)
+    {
+        result = d(i);
+        if (result < 10001)
+            list[result] = 1;
+    }
+    for (int j = 0; j < 10001; j++)
+    {
+        if (list[j] != 1)
+            printf("%d\n", j);
+    }
 }
